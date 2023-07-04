5527273e6a07e5cea41edda8a4f3b58f009176dc
Nathan Glenn <garfieldnate@gmail.com>
2023-07-04 03:38:05 -0500

    Initial commit; don't know if it works

---
+++ predict.py
@@ -0,0 +1,27 @@
+import json
+
+from cog import BasePredictor, Path, Input
+import stable_whisper
+
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        self.model = stable_whisper.load_model(model='large-v2')
+
+    def predict(self,
+            audio: Path = Input(description="Audio to transcribe"),
+            language: str = Input(default="en", description="Language to transcribe"),
+    ) -> Path:
+        result = self.model.transcribe(
+            audio,
+            language=language,
+            demucs=True,
+            regroup=True,
+            temperature_increment_on_fallback=0.2,
+            # **decode_options
+            beam_size=5,
+            best_of=5,
+            )
+        output = json.dumps(result, allow_nan=True)
+        return output
