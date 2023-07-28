9744093cfd9c74fac39017c692166129ae915835
Nathan Glenn <garfieldnate@gmail.com>
2023-07-28 10:28:05 -0500

    Match parameters with original server params
    We used VAD but not demucs. Just make everything we've used a parameter
    so we can experiment after deployment.

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@
 
 import json
 import os
+from typing import Union
 
 from cog import BasePredictor, Path, Input
 import stable_whisper
@@ -38,14 +39,21 @@ class Predictor(BasePredictor):
     def predict(self,
             audio_path: Path = Input(description="Audio to transcribe"),
             language: str = Input(default="en", description="Language to transcribe"),
+            demucs: bool = Input(default=False, description="Whether to preprocess the audio track with Demucs to isolate vocals/remove noise."),
+            vad: bool = Input(default=True, description="Whether to use Silero VAD to generate timestamp suppression mask."),
+            beam_size: int = Input(default=5, description="Number of beams in beam search, only applicable when temperature is zero."),
+            best_of: int = Input(default=5, description="Number of candidates when sampling with non-zero temperature."),
+            regroup: Union[bool, str] = Input(default=True, description="Whether to regroup all words into segments with more natural boundaries. "
+                                              "Specify string for customizing the regrouping algorithm."),
     ) -> str:
         result = self.model.transcribe(
             str(audio_path),
             language=language,
-            demucs=True,
-            regroup=True,
-            beam_size=5,
-            best_of=5,
+            demucs=demucs,
+            vad=vad,
+            regroup=regroup,
+            beam_size=beam_size,
+            best_of=best_of,
             )
         # Adapted from stable_whisper/text_output.py; original can only save to file
         if not isinstance(result, dict) and callable(getattr(result, 'to_dict')):
