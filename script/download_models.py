4e5417c01adc12c950b3aed9c5c895867e6e2b59
Nathan Glenn <garfieldnate@gmail.com>
2023-11-28 22:37:43 -0600

    Revert "pre-download demucs model"
    This reverts commit 0fd812cefff4fbfe155b9986a8dbcbc9ecd132cc.
    
    The recognition results ended up being really bad, so I'll just revert
    this until I can investigate further.
    
    Shrek test file was giving "Thank you for watching." over and over
    again.

--- script/download_models.py
+++ script/download_models.py
@@ -11,12 +11,4 @@ from predict import Predictor
 # Running prediction once will trigger the download of the model
 p = Predictor()
 p.setup()
-# VAD and demucs are their own models, so we enable them to trigger their download
-p.predict(
-    audio_path=Path('testing-1-2-3.mp3'),
-    language='en',
-    best_of=1,
-    beam_size=1,
-    regroup=True,
-    vad=True,
-    demucs=True)
+p.predict(audio_path=Path('testing-1-2-3.mp3'), language='en', best_of=1, beam_size=1, regroup=True, vad=True, demucs=False)
