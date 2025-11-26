a54ee3f7727f77d8b3f7cd04fba1a0e64f48083f
Kai Chang <kaijchang@gmail.com>
2025-11-26 15:44:56 -0800

    fix types

--- predict.py
+++ predict.py
@@ -62,7 +62,7 @@ class Predictor(BasePredictor):
             best_of: int = Input(default=5, description="Number of candidates when sampling with non-zero temperature (transcribe mode only)."),
             regroup: bool = Input(default=True, description="Whether to regroup all words into segments with more natural boundaries."),
             initial_prompt: str = Input(default=None, description="Text to provide as a prompt for the first window (transcribe mode only)."),
-            aligner: str | dict = Input(default='new', choices=['new', 'legacy'], description="The aligner to use."),
+            aligner: str = Input(default='new', choices=['new', 'legacy'], description="The aligner to use."),
             suppress_arabic_numerals: bool = Input(default=True, description="Whether to suppress Arabic numerals."),
             suppress_pronounceable_symbols: bool = Input(default=True, description="Whether to suppress pronounceable symbols."),
     ) -> str:
@@ -75,7 +75,7 @@ class Predictor(BasePredictor):
             if text == "":
                 raise ValueError("text parameter is required when mode='align'")
             if aligner == "new":
-                aligner = {"char_split": True}
+                aligner = {"char_split": True}  # pyright: ignore[reportAssignmentType]
             result = typing.cast(nn.Module, self.model.align)(
                 str(audio_path),
                 text=text,
